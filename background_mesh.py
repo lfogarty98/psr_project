f04e426469ece5569a17c429eb4bd5d373cd9574
Liam Fogarty <lfogarty9995@gmail.com>
2025-02-10 20:33:12 +0100

    push working tetgen background mesh example
    Signed-off-by: Liam Fogarty <lfogarty9995@gmail.com>

--- background_mesh.py
+++ background_mesh.py
@@ -1,12 +1,10 @@
 import pyvista as pv
 import tetgen
 import numpy as np
-
-# Load or create your PLC
-sphere = pv.Sphere(theta_resolution=10, phi_resolution=10)
+import polyscope as ps
 
 # Generate a background mesh with desired resolution
-def generate_background_mesh(bounds, resolution=20, eps=1e-6):
+def generate_background_mesh(bounds, resolution=30, eps=1e-6):
     x_min, x_max, y_min, y_max, z_min, z_max = bounds
     grid_x, grid_y, grid_z = np.meshgrid(
         np.linspace(x_min - eps, x_max + eps, resolution),
@@ -16,16 +14,30 @@ def generate_background_mesh(bounds, resolution=20, eps=1e-6):
     )
     return pv.StructuredGrid(grid_x, grid_y, grid_z).triangulate()
 
-bg_mesh = generate_background_mesh(sphere.bounds)
-
 # Define sizing function based on proximity to a point of interest
 def sizing_function(points, focus_point=np.array([0, 0, 0]), max_size=1.0, min_size=0.1):
-    distances = np.linalg.norm(points - focus_point, axis=1)
+    distances = 1 / (np.linalg.norm(points - focus_point, axis=1) + 1e-16)
     return np.clip(max_size - distances, min_size, max_size)
 
+
+# Load or create your PLC
+sphere = pv.Sphere(radius=1.5, center=(0, 0, 0))
+print(f'Sphere bounds: {sphere.bounds}')
+
+# Create a background mesh
+bg_mesh = generate_background_mesh(sphere.bounds)
 bg_mesh.point_data['target_size'] = sizing_function(bg_mesh.points)
 
+# Compute tetrahedralization
 tet_kwargs = dict(order=1, mindihedral=20, minratio=1.5)
 tet = tetgen.TetGen(sphere)
-tet.tetrahedralize(bgmesh=bg_mesh, **tet_kwargs)
-refined_mesh = tet.grid
+nodes, elems = tet.tetrahedralize(bgmesh=bg_mesh, **tet_kwargs)
+# refined_mesh = tet.grid
+
+print(f'nodes: {nodes.shape}, elems: {elems.shape}')
+
+ps.init()
+
+sphere_mesh = ps.register_volume_mesh('sphere', nodes, elems)
+
+ps.show()
